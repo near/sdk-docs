e0595c1d88a0626efece98fc268a47b9a05a5e3b
Roopchand <roop.nsa@gmail.com>
2022-02-28 18:08:25 +0530

    React live editor added to test near-api-js api's

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,109 +1,118 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
-  title: 'NEAR SDK docs',
-  tagline: 'Write smart contracts to run on the NEAR blockchain!',
-  url: 'https://near-sdk.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'NEAR',
-  projectName: 'sdk-docs',
-  themes: [
-    '@saucelabs/theme-github-codeblock'
-  ],
-  themeConfig: {
-    algolia: {
-      appId: "QPBQ67WNIG",
-      apiKey: "b590ae1153bf574215ca1605c19eb1fe",
-      indexName: "near",
-      searchParameters: {
-        // search only on near-sdk.io
-        facetFilters: ["tags:sdk"]
-      }
-    },
-    prism: {
-      additionalLanguages: ['rust'],
-    },
-    colorMode: {
-      defaultMode: 'dark',
-    },
-    navbar: {
-      title: '',
-      logo: {
-        alt: 'NEAR logo',
-        src: 'img/near_logo.svg',
-        srcDark: 'img/near_logo_white.svg',
-      },
-      items: [
-        {
-          href: 'https://docs.rs/near-sdk/',
-          label: 'docs.rs',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/near/near-sdk-rs',
-          label: 'Rust SDK GitHub',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/near/sdk-docs',
-          label: 'Docs GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/nearprotocol',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.com/invite/UY9Xf2k',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/NEARProtocol',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'SDK GitHub',
-              href: 'https://github.com/near/sdk-docs',
-            },
-          ],
-        },
-      ],
-      copyright: `${new Date().getFullYear()} NEAR Protocol | All rights reserved | hello@near.org`,
-    },
-    gtag: {
-      trackingID: 'G-NEHEBVDQKL',
-      anonymizeIP: true,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/near/sdk-docs/edit/main/',
-          routeBasePath: '/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+    title: 'NEAR SDK docs',
+    tagline: 'Write smart contracts to run on the NEAR blockchain!',
+    url: 'https://near-sdk.io/',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'NEAR',
+    projectName: 'sdk-docs',
+    themes: [
+        '@docusaurus/theme-live-codeblock'
     ],
-  ],
+    themeConfig: {
+        algolia: {
+            appId: "QPBQ67WNIG",
+            apiKey: "b590ae1153bf574215ca1605c19eb1fe",
+            indexName: "near",
+            searchParameters: {
+                // search only on near-sdk.io
+                facetFilters: ["tags:sdk"]
+            }
+        },
+        prism: {
+            additionalLanguages: ['rust'],
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+        colorMode: {
+            defaultMode: 'dark',
+        },
+        navbar: {
+            title: '',
+            logo: {
+                alt: 'NEAR logo',
+                src: 'img/near_logo.svg',
+                srcDark: 'img/near_logo_white.svg',
+            },
+            items: [
+                {
+                    to: '/editor',
+                    label: 'Editor',
+                    position: 'right',
+                },
+                {
+                    href: 'https://docs.rs/near-sdk/',
+                    label: 'docs.rs',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/near/near-sdk-rs',
+                    label: 'Rust SDK GitHub',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/near/sdk-docs',
+                    label: 'Docs GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/nearprotocol',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.com/invite/UY9Xf2k',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/NEARProtocol',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'SDK GitHub',
+                            href: 'https://github.com/near/sdk-docs',
+                        },
+                    ],
+                },
+            ],
+            copyright: `${new Date().getFullYear()} NEAR Protocol | All rights reserved | hello@near.org`,
+        },
+        // gtag: {
+        //     trackingID: 'G-NEHEBVDQKL',
+        //     anonymizeIP: true,
+        // },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl:
+                        'https://github.com/near/sdk-docs/edit/main/',
+                    routeBasePath: '/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
 };
